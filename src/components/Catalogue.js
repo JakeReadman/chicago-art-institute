9e024f461cc4803915e137db5faf2564fb02edd4
Jake Readman <jake.readman@consultmyapp.com>
2022-06-16 16:40:06 +0100

    set up catalogue

---
+++ src/components/Catalogue.js
@@ -0,0 +1,45 @@
+import { Pagination } from 'antd';
+import 'antd/dist/antd.css';
+import React, { useEffect, useState } from 'react';
+import { getRequest } from '../library/middleware';
+import Page from './Page';
+
+export default function Catalogue() {
+    const [item, setItem] = useState({});
+    const [pagination, setPagination] = useState(1);
+
+    useEffect(() => {
+        getRequest({
+            endpoint: '/v1/products',
+            params: {
+                limit: 1,
+            },
+            onSuccess: (response) => {
+                setItem(response.data.data[0]);
+                setPagination(response.data.pagination);
+            },
+            onError: (error) => {
+                console.log(error);
+            },
+        });
+    }, []);
+
+    const changePage = (page) => {
+        getRequest({
+            endpoint: '/v1/products',
+            params: {
+                limit: 1,
+                page: page,
+            },
+            onSuccess: (response) => {
+                setItem(response.data.data[0]);
+                setPagination(response.data.pagination);
+            },
+            onError: (error) => {
+                console.log(error);
+            },
+        });
+    };
+
+    return <div></div>;
+}
